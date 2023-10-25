38c00ad55365c7ed4d9229840d0f68bf557d568f
Iqbal Mahdi <mahdiiqbal37@gmail.com>
2023-10-26 00:34:30 +0600

    README updated and hompage slightly changed

--- Frontend/home.php
+++ Frontend/home.php
@@ -4,19 +4,18 @@
     $users = getUsers();
 ?>
 
-<h3 class="ui blue header centered">Hello There, <i><?php echo isset($_SESSION["username"]) ? '@' . $_SESSION["username"] : "guest"; ?></i></h3>
-
 <div style="margin: 10vh 0">
+    <h3 class="ui blue header centered">Hello There, <i><?php echo isset($_SESSION["username"]) ? '@' . $_SESSION["username"] : "guest"; ?></i></h3>
+
+    <?php if (isset($_SESSION["isLoggedIn"])): ?>
     <h2 class="ui header centered">Users</h2>
-    
+
     <table class="ui celled striped teal table">
         <thead>
             <tr>
                 <th>UserName</th>
-                <?php if (isset($_SESSION["isLoggedIn"])): ?>
-                    <th>Email</th>
-                    <th>Role</th>
-                <?php endif; ?>
+                <th>Email</th>
+                <th>Role</th>
                 <?php if (isset($_SESSION["role"]) && $_SESSION["role"] == "admin"): ?>
                     <th>Action</th>
                 <?php endif; ?>
@@ -28,14 +27,12 @@
                     <td>
                         <?php echo "@{$user->username}"; ?>
                     </td>
-                    <?php if (isset($_SESSION["isLoggedIn"])): ?>
-                        <td>
-                            <?php echo $user->email; ?>
-                        </td>
-                        <td>
-                            <?php echo $user->role; ?>
-                        </td>
-                    <?php endif; ?>
+                    <td>
+                        <?php echo $user->email; ?>
+                    </td>
+                    <td>
+                        <?php echo $user->role; ?>
+                    </td>
                     <?php if (isset($_SESSION["role"]) && $_SESSION["role"] == "admin"): ?>
                         <td>
                             <a href="/manage-user-role/<?php echo $userId; ?>">Manage Role</a>
@@ -51,4 +48,7 @@
             <?php endforeach; ?>
         </tbody>
     </table>
+    <?php else: ?>
+        <h2 class="ui header centered">Log in to view users.</h2>
+    <?php endif; ?>
 </div>
--- README.md
+++ README.md
@@ -11,3 +11,28 @@
 #### Regular User
 * username : JuneDoe
 * password : 123123
+
+## SERVE THE PROJECT
+
+`php -S localhost:8000`
+
+## URL ENDPOINTS AND ACCESSIBILITY
+
+#### Accessible to all
+* <http://localhost:8000/>
+
+#### Accessible to unauthenticated users
+* <http://localhost:8000/register/>
+* <http://localhost:8000/login/>
+
+#### Accessible to all authenticated users
+* <http://localhost:8000/profile/>
+* <http://localhost:8000/user-area/>
+* <http://localhost:8000/logout/>
+
+#### Accessible to managers and admins
+* <http://localhost:8000/manager-area/>
+
+#### Accessible to admins only
+* <http://localhost:8000/admin-area/>
+* <http://localhost:8000/manage-user-role/{userId}>
